e5b50aef1b3e1e2279eb41217298598ca3c875b0
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-21 13:52:14 +0100

    Compiling errors fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormComponent } from './form/form.component';
 import { ExamComponent } from './exam/exam.component';
 
 const routes: Routes = [
   { path: '', redirectTo: './app.component', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'form', component: FormComponent },
   { path: 'exam', component: ExamComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatSliderModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -13,7 +12,6 @@ import { ExamComponent } from './exam/exam.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
     LoginComponent,
     RegisterComponent,
     ExamComponent
